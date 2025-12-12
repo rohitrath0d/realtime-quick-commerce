7fc0c4ebf3d1de264e52e6ce46943522250f6b27
Rohit <rohit.rthd.04@gmail.com>
2025-12-12 21:46:41 +0530

    adding socket connection configuration and basic server setup connection with health check boundary

--- backend-scripts/src/runServer.js
+++ backend-scripts/src/runServer.js
@@ -1,13 +1,54 @@
 import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-const port= process.env.port || 4000;
-
-app.get('/', (req, res)=> {
-  res.send('quick comm backend up and running: /home-route')
+const httpServer = http.createServer(app);
+// const socketConnection= socketIO(httpServer)
+const socketConnection = new Server(httpServer, {
+  cors: {
+    origin: '*'  // for development
+  }
 })
 
-app.listen(port, ()=> {
+const port = process.env.port || 4000;
+
+app.get('/', (req, res) => {
+  res.send('quick comm backend up and running: /home-route')
+});
+
+app.get('/health', (req, res) => res.send({
+  status: 'health check successful',
+  time: new Date().toISOString()
+}));
+
+// init socket connection
+socketConnection.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Server',
+    text: 'Welcome',
+    createdAt: Date.now()
+  });
+
+  socket.on('createMessage', (message)=> {
+    console.log('New message:', message);
+    socketConnection.emit('newMessage', message)    // send to everyone
+  });
+});
+
+// attach io so routes/controllers can emit
+app.set('io', socketConnection);
+
+// app.listen(port, ()=> {      // app -> server (for socket connection)
+httpServer.listen(port, () => {
   console.log(`Server is running on port: http://localhost:${port}`)
 })
